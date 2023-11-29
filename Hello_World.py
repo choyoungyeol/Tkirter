747e99a849ea04205899f366031b4259374cd521
Young-Yeol Cho <60500365+choyoungyeol@users.noreply.github.com>
2023-11-29 13:16:17 +0900

    Create Hello_World.py

---
+++ Hello_World.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+root.geometry("500x500")
+root.title("My First GUI")
+
+label = tk.Label(root, text="Hello World !", font=("Arial", 20))
+
+label.pack()
+
+root.mainloop()
